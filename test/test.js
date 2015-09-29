ef446fbef4e1643d9a06254ae2fc7f1c8727b83d
Gabriele <gabriele.s@21gear.com>
2015-09-29 16:28:04 +0200

    Restructure of folders and files

--- test/test.js
+++ test/test.js
@@ -1,1 +1,8 @@
-// todo
+var epsilon = require('./lib/epsilon.js');
+
+var source = "teststring";
+var target = "anotherstring";
+
+var result = epsilon.leven(source,target);
+
+console.log(result);
